656f77dde4217a7b1d4fba31f8c282fbd1d0279b
unknown <koyyanarahul4002@gmail.com>
2025-01-07 23:16:19 +0530

    episode 01

---
+++ app.js
@@ -0,0 +1,69 @@
+// const heading = React.createElement('h1',{id:"heading", xyz:"abc"},"hello rahul");
+
+// console.log(heading); //react element which is nothing but js object
+
+
+
+{/* <div id="parent">
+     <div id="child">
+          <h1>hello react from h1</h1>
+     </div>
+</div> */}
+
+
+{/* <div id="parent">
+     <div id="child">
+          <h1>hello react from h1</h1>
+          <h2>hello react from h2</h2>
+     </div>
+</div> */}
+
+
+{/* <div id="parent">
+     <div id="child">
+          <h1>hello react from h1</h1>
+          <h2>hello react from h2</h2>
+     </div>
+     <div id="child2">
+          <h1>hello react from h1</h1>
+          <h2>hello react from h2</h2>
+     </div>
+</div> */}
+
+
+const heading = React.createElement(
+     "div",
+     {id: "parent"},
+     [React.createElement(
+          "div",
+          {id: "child"},
+          [React.createElement(
+               "h1",
+               {},
+               "hello react from h1"
+          ),
+          React.createElement(
+               "h2",
+               {},
+               "hello react from h2"
+          )]
+     ),
+     React.createElement(
+          "div",
+          {id: "child2"},
+          [React.createElement(
+               "h1",
+               {},
+               "hello react from h1"
+          ),
+          React.createElement(
+               "h2",
+               {},
+               "hello react from h2"
+          )]
+     ) ]
+)
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(heading);
